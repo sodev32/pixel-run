a565acaa4cd7d72120562c73af800f4f658217e7
sodev32 <so_n_dev32@outlook.jp>
2026-07-20 07:31:25 +0900

    feat: add health check endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Pixel Run Backend Started!')
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get('/health',(_req,res)=>{
+    res.send('OK')
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
