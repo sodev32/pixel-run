94ec1553688e4bd7a950df0b784bfd361d4dc232
sodev32 <so_n_dev32@outlook.jp>
2026-07-27 07:40:10 +0900

    dev:create POST/score and GET/score

--- backend/src/routes/score.ts
+++ backend/src/routes/score.ts
@@ -1,11 +1,24 @@
 import { Router } from 'express';
 
+type Score = {
+  playerName: string;
+  time: number;
+};
+
 const router = Router();
+const scores: Score[] = [];
 
 router.post('/', (req, res) => {
-  const { playerName } = req.body;
-  console.log(req.body);
+  const { playerName, time } = req.body;
+  const score: Score = { playerName, time };
+  scores.push(score);
+  console.log(score);
   res.status(200).json({ message: `Score received from ${playerName}` });
+});
+
+router.get('/', (_req, res) => {
+  res.status(200).json(scores);
+  console.log(scores);
 });
 
 export default router;
