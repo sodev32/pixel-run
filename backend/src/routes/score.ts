ae1dfd616237a17c54dcc474aa1798904fa140f9
sodev32 <so_n_dev32@outlook.jp>
2026-07-28 07:22:59 +0900

    dev:scoreApi if create

--- backend/src/routes/score.ts
+++ backend/src/routes/score.ts
@@ -10,13 +10,25 @@ const scores: Score[] = [];
 
 router.post('/', (req, res) => {
   const { playerName, time } = req.body;
+
+  if (!playerName) {
+    return res.status(400).json({ message: 'Player name is required' });
+  }
+
+  if (typeof time !== 'number') {
+    return res
+      .status(400)
+      .json({ message: 'Time is required and must be a number' });
+  }
+
   const score: Score = { playerName, time };
   scores.push(score);
   console.log(score);
-  res.status(200).json({ message: `Score received from ${playerName}` });
+  res.status(201).json({ message: `Score received from ${playerName}` });
 });
 
 router.get('/', (_req, res) => {
+  scores.sort((a, b) => a.time - b.time);
   res.status(200).json(scores);
   console.log(scores);
 });
